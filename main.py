bac1a1d567d25036681dcd73dad0e1210c65ae78
cody <kenleybeast1@gmail.com>
2022-12-07 23:24:19 +0800

    Updated pyauto gui

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ import time
 def main():
     #print res of screen
     print(pyautogui.size()) 
+    
     #prints where the mouse current position is
     print(pyautogui.position())
+    
     #moves the mouse over time(x coordinate,y coordinate,seconds take to move)
     # pyautogui.moveTo(100,1000,3)
+    
     #moves the mouse relative to its position
-    pyautogui.moveRel(100,100,3)
+    # pyautogui.moveRel(100,100,3)
+    
     #Perform a click python
-    print("hello")
+    # pyautogui.click(clicks=1,button="PRIMARY")
+    # pyautogui.doubleClick()
+    # pyautogui.tripleClick()
+    
+    #Scroll UP
+    pyautogui.scroll(500)
+    
+    #ScrollDown
+    pyautogui.scroll(500)
+    print(pyautogui.FAILSAFE)
 main()
