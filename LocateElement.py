e8e1f504203714fd8f1463a92748e024e13ffc54
Kenley So <kenleybeast1@gmail.com>
2022-12-08 23:38:22 +0800

    Added new pyAutoGui

--- LocateElement.py
+++ LocateElement.py
@@ -6,8 +6,13 @@ def locate_image():
     cords_center = pyautogui.center(cords_image)
     print(cords_center)
     pyautogui.click(cords_center[0],cords_center[1])
-    
+
+def chromeLaunch():
+    chromeImageCords = pyautogui.locateOnScreen(r"C:\Users\kenle\Desktop\PyAutoGUI\PyAutoGUI\Images\Chrome.png",confidence=.8)
+    chromeCenter = pyautogui.center(chromeImageCords)
+    pyautogui.click(chromeCenter[0],chromeCenter[1])
     
 def main():
-    locate_image()
+    chromeLaunch()
+    # locate_image()
 main()
